851b71999b131a47bf87e192767644b60d8d42a4
Hanieh.ZiaFarshchi <111128760+HaniehZiaFarshchi98@users.noreply.github.com>
2022-08-28 19:57:39 +0430

    Update 6.BMI.py

--- 6.BMI.py
+++ 6.BMI.py
@@ -1,17 +1,18 @@
-from telnetlib import BM
-
-
 height= float(input("please enter your height(m):"))
 weight= float(input("please enter yor weight(kg):"))
 BMI= (weight/(height*height))
 if(BMI > 40):
-    print(BMI , "you must go on a diet!")
+    print(BMI , "you have level 3 obesity!")
 elif (35 <= BMI <= 40):
-     print(BMI , "you can go on a diet!")
+     print(BMI , "you have level 2 obesity!")
 elif(30 <= BMI < 35):
-    print(BMI , "you are normal! be carefull")
+    print(BMI , "you have level 1 obesity!")
 elif(24 <= BMI < 30):
-     print(BMI , "you ae really skinny!")
+     print(BMI , "you are overweight!")
+elif(18.5 <= BMI < 24):
+    print(BMI , "you are normal! be carefull")
+elif(16 <= BMI < 18.5):
+    print(BMI , "you are underweight! ")
 else:
     print("try again!")
 
