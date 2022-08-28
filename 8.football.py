4d56a12831edb1d2237668a9756f7ef9315854ef
Hanieh.ZiaFarshchi <111128760+HaniehZiaFarshchi98@users.noreply.github.com>
2022-08-28 20:00:13 +0430

    Update 8.football.py

--- 8.football.py
+++ 8.football.py
@@ -1,12 +1,10 @@
-from unittest import result
-
-
 scores=0
 wins=0
 draws=0
 loses=0
 result=0
-for i in range (8):
+count=0
+for i in range(8):
     print("result:" ,scores , "1:wins 2:draws 3:loses")
     result=input()
     if result == "1":
@@ -16,4 +14,5 @@ for i in range (8):
     elif result == "3":
         scores +=0
     else:
-        print("enter the result again!")    
+        print("enter the result again!")  
+print ("final score=" , scores)          
